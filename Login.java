74244a65b5c789a4aa67b220414db3edf9b0f3ea
arpit <nigamarpit7000@gmail.com>
2021-04-12 16:48:36 +0530

    first commit

---
+++ Login.java
@@ -0,0 +1,15 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class Login
+{
+    public static void main(final String[] array) {
+        final LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setTitle("Login Form");
+        loginFrame.setVisible(true);
+        loginFrame.setBounds(10, 10, 370, 600);
+        loginFrame.setDefaultCloseOperation(3);
+        loginFrame.setResizable(false);
+    }
+}
